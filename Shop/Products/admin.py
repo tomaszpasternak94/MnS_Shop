bca2c5b9c26a8a185499b0fd597ae30afb5d9fcf
Tomek <t.pasternak94@gmail.com>
2020-08-11 21:54:49 +0200

    feat(3) : model ProductTable created and added to a settings file. ProductTable registered in admin file. Added function returning products names in the administration panel.

--- Shop/Products/admin.py
+++ Shop/Products/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import ProductsTable
 
 # Register your models here.
+
+admin.site.register(ProductsTable)
--- Shop/Products/models.py
+++ Shop/Products/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class ProductsTable(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=False)
+    price = models.DecimalField(max_digits=12, decimal_places=2)
+    # This function returns names of products in the admin panel
+    def __str__(self):
+        return self.name
