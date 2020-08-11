7a5cd1ec9a3acff8353935075f0c231ad1210b2f
Tomek <t.pasternak94@gmail.com>
2020-08-11 22:08:23 +0200

    feat(4) : index views added (all products name from db on a homepage). Urls homepage with index view registered.

--- Shop/Products/views.py
+++ Shop/Products/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from .models import ProductsTable
+from django.http import HttpResponse
 # Create your views here.
+
+# Select * from ProductsTable - list of products on a homepage
+def index(request):
+    query = ProductsTable.objects.all()
+    return HttpResponse(query)
